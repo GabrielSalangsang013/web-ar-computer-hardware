e4f8991245fc9a243ebd64c78e5edb021d42ec32
GabrielSalangsang013 <gabrielsalangsang013@gmail.com>
2022-11-06 03:31:49 +0800

    v1.5.24 webar performance test 16

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Markerless Web based Augmented Reality Technology for Information Technology Course Visualizing Computer Hardware Login Page">
 	<meta name="baseURL" content="{{ url('/') }}">
     <link rel="icon" type="image/png" href="https://res.cloudinary.com/dr9p65xlj/image/upload/c_scale,q_10,w_20/v1667634716/images/ualogo_triinr.webp"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/GabrielSalangsang013/team-cord-web-ar@33478f2/public/css/index.min.css">
