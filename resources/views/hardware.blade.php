cf9f3a5b2e9c0eac5b0a8011bb50cda93b5d5fe4
GabrielSalangsang013 <gabrielsalangsang013@gmail.com>
2022-11-05 23:40:28 +0800

    v1.5.24 webar performance test 7

--- resources/views/hardware.blade.php
+++ resources/views/hardware.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="https://res.cloudinary.com/dr9p65xlj/image/upload/c_scale,q_10,w_20/v1667634716/images/ualogo_triinr.webp"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/GabrielSalangsang013/team-cord-web-ar@latest/public/css/hardware.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/GabrielSalangsang013/team-cord-web-ar@master/public/css/hardware.min.css">
     <script type="module" src="https://cdn.jsdelivr.net/npm/@google/model-viewer@0.6.0/dist/model-viewer.min.js"></script>
     <script nomodule src="https://cdn.jsdelivr.net/npm/@google/model-viewer@0.6.0/dist/model-viewer-legacy.min.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
